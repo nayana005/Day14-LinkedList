1604d7d63beaca735d99a36e0ea286683e80994b
nayana005 <nayanasuresh1005@gmail.com>
2022-08-29 01:47:12 +0530

    Added: Pop last

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -5,17 +5,20 @@ public class LinkedListMain {
 	public static void main(String[] args) {
 
 		LinkedList<Integer> linkedList1 = new LinkedList<>();
-		linkedList1.push(70);
-		linkedList1.push(56);
-		linkedList1.insert(30);
-		System.out.println(linkedList1.pop());
-		linkedList1.print();
+        linkedList1.push(70);
+        linkedList1.push(56);
+        linkedList1.insert(30);
+        System.out.println(linkedList1.pop());
+        linkedList1.print();
+        System.out.println();
 
-		LinkedList<Integer> linkedList2 = new LinkedList<>();
-		linkedList2.add(56);
-		linkedList2.add(30);
-		linkedList2.add(70);
-		linkedList2.print();
-
-	}
+        LinkedList<Integer> linkedList2 = new LinkedList<>();
+        linkedList2.add(56);
+        linkedList2.add(30);
+        linkedList2.add(70);
+        linkedList2.print();
+        System.out.println();
+        System.out.println(linkedList2.popLast());
+        linkedList2.print();
+    }
 }
