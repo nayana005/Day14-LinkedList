a9e80c062c17601c061714845b686c0506f17787
nayana005 <nayanasuresh1005@gmail.com>
2022-08-30 03:37:03 +0530

    Added: Ordered Linked List

--- src/com/bridgelabz/Node.java
+++ src/com/bridgelabz/Node.java
@@ -1,27 +1,11 @@
 package com.bridgelabz;
 
 public class Node<K> {
-   
-	private K key;
-    private Node<K> next;
+    int data;
+    Node<K> next;
 
-    public Node(K key) {
-        this.key = key;
-    }
-
-    public K getKey() {
-        return key;
-    }
-
-    public void setKey(K key) {
-        this.key = key;
-    }
-
-    public Node<K> getNext() {
-        return next;
-    }
-
-    public void setNext(Node<K> next) {
-        this.next = next;
+    Node(int data) {
+        this.data = data;
+        this.next = null;
     }
 }
